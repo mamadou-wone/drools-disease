b795eafece7eb25a380b94440da76712ff8ef965
Mamadou WONE <wone.dev@gmail.com>
2021-10-25 16:00:20 +0000

    process

--- models/diagnostic.py
+++ models/diagnostic.py
@@ -9,11 +9,11 @@ from python_drools_sdk.kie.drools import Drools
 from python_drools_sdk.utils import helpers
 from python_drools_sdk.exceptions.drools_exception import DroolsException
 
-Drools.KIE_SERVER_CONTAINER_PACKAGE = 'com.myspace.ministere' 
+Drools.KIE_SERVER_CONTAINER_PACKAGE = 'com.myspace.tanoor' 
 Drools.KIE_SERVER_USERNAME = 'admin'
 Drools.KIE_SERVER_PASSWORD = 'admin'
 Drools.KIE_SERVER_ROOT_URL = 'http://172.17.0.1:8180'
-Drools.KIE_SERVER_CONTAINER_ID = 'ministere_1.0.0-SNAPSHOT' 
+Drools.KIE_SERVER_CONTAINER_ID = 'tanoor_1.0.0-SNAPSHOT' 
 
 class Diagnostic(models.Model):
     _name = 'smart_form.diagnostic'
@@ -28,21 +28,77 @@ class Diagnostic(models.Model):
     disease_ids = fields.Many2many("smart_form.disease", string="Maladies", readonly=True)
     
     def get_diagnostic(self):
-        symptomsList = []
+        symptoms_list = []
+        diagnostics = []
         for record in self:
             for symtom in record.symptom_ids:
                 name = symtom.name
-                symptoms = symptom.Symptom("-".join(name.split(" ")))
-                symptomsList.append(symptoms.name)
-        # symptomsList = ",".join(symptomsList)        
-        decision = diagnostic.Diagnostic(symptoms=symptomsList)
-        insert_command = InsertObjectCommand(object=decision, out_identifier="decision").initialize()
-        Drools.add_command(insert_command)     
+                symptoms_list.append(name)
+                
+        diagnostics = [diagnostic.Diagnostic(symptoms=[item]) for item in symptoms_list]        
+        # decision = diagnostic.Diagnostic(symptoms=[name])
+        # diagnostics.append(decision)
+        print(diagnostics)
+        
+        print(symptoms_list)         
+        insert_elements_command = InsertElementsCommand(objects=diagnostics, out_identifier='decision').initialize()
+
+        Drools.add_command(insert_elements_command)
+
         try:
             response = Drools.execute_commands()
         except DroolsException as de:
             print(de)
-
+            
         drools_diagnostic_response = response['decision']
-        diseases = [self.env["smart_form.disease"].create({"name": item.split(",")[0], "score": item.split(",")[1]}) for item in drools_diagnostic_response['diseases']]
-        self.disease_ids = [(6, 0, [disease.id for disease in diseases])]    
+        print(drools_diagnostic_response)
+        
+        diseases = [data["diseases"] for data in drools_diagnostic_response]
+        min_length = min([len(data["diseases"]) for data in drools_diagnostic_response])
+        
+        result = []
+        score = []
+        for item in diseases:
+            for i in range(min_length):
+                result.append(item[i].split(",")[0])
+                score.append(item[i].split(",")[1])
+          
+        other_disease = []      
+        other_score = []   
+        for item in diseases:
+            for i in range(min_length ,len(item)):
+                other_disease.append(item[i].split(",")[0])
+                other_score.append(item[i].split(",")[1])  
+
+        result = result + other_disease
+        score = score + other_score
+        
+        final_score = []
+        final_list = []
+        for d in range(len(result)):
+            if result[d] not in final_list:
+                final_score.append(score[d])
+                final_list.append(result[d])
+            else:
+                final_score[final_list.index(result[d])] = int(final_score[final_list.index(result[d])]) + int(score[d])
+        
+        print(final_list)
+        print(final_score) 
+        
+        diseases = [self.env["smart_form.disease"].create({"name": final_list[item], "score": final_score[item]}) for item in range(len(final_list))]
+        self.disease_ids = [(6, 0, [disease.id for disease in diseases])]  
+        # drools_diagnostic_response = []
+        
+        # print(result)
+        # print(score)        
+        # decision = diagnostic.Diagnostic(symptoms=symptomsList)
+        # insert_command = InsertObjectCommand(object=decision, out_identifier="decision").initialize()
+        # Drools.add_command(insert_command)     
+        # try:
+        #     response = Drools.execute_commands()
+        # except DroolsException as de:
+        #     print(de)
+
+        # drools_diagnostic_response = response['decision']
+        # diseases = [self.env["smart_form.disease"].create({"name": item.split(",")[0], "score": item.split(",")[1]}) for item in drools_diagnostic_response['diseases']]
+        # self.disease_ids = [(6, 0, [disease.id for disease in diseases])]    
